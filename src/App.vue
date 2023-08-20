c2fb3ef1a40d28fb05fa530fc282c6e0101bdc1a
m0slah <muhammedslahqadir@gmail.com>
2023-08-20 11:12:20 +0300

    update aside list

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,11 @@
         class="flex items-center gap-x-4 px-8 text-2xl font-medium text-white focus:outline-none"
       >
         <img
-          :src="DashboardIcon"
+          :src="dashboardIcon"
           alt="dashboardIcon"
           class="h-6 w-6 stroke-current"
         />
-        <span>Transfer</span>
+        <span> Transfer</span>
       </a>
       <ul class="flex flex-1 flex-col gap-y-10 px-8 pt-14">
         <li v-for="item in menu">
@@ -27,22 +27,29 @@
   </div>
 </template>
 
-<script setup>
-import HomeIcon from "./assets/icons/home.svg";
-import BillIcon from "./assets/icons/bill.svg";
-import InvoiceIcon from "./assets/icons/invoice.svg";
-import BuildingIcon from "./assets/icons/building.svg";
-import CardIcon from "./assets/icons/card.svg";
-import SettingsIcon from "./assets/icons/settings.svg";
+<script>
+import HomeIcon from "@/assets/icons/home.svg"; // Update the path to your SVG icon files
+import BillIcon from "@/assets/icons/bill.svg"; // Update the path to your SVG icon files
+import InvoiceIcon from "@/assets/icons/invoice.svg"; // Update the path to your SVG icon files
+import BuildingIcon from "@/assets/icons/building.svg"; // Update the path to your SVG icon files
+import CardIcon from "@/assets/icons/card.svg"; // Update the path to your SVG icon files
+import SettingsIcon from "@/assets/icons/settings.svg"; // Update the path to your SVG icon files
+import DashboardIcon from "@/assets/icons/dashboard.svg";
 
-import DashboardIcon from "./assets/icons/dashboard.svg";
-
-const menu = [
-  { name: "Home", icon: HomeIcon },
-  { name: "USA Payors", icon: BillIcon },
-  { name: "Invoices", icon: InvoiceIcon },
-  { name: "My Banks", icon: BuildingIcon },
-  { name: "Withdraw", icon: CardIcon },
-  { name: "Settings", icon: SettingsIcon },
-];
+export default {
+  name: "App",
+  data() {
+    return {
+      menu: [
+        { name: "Home", icon: HomeIcon },
+        { name: "USA Payors", icon: BillIcon },
+        { name: "Invoices", icon: InvoiceIcon },
+        { name: "My Banks", icon: BuildingIcon },
+        { name: "Withdraw", icon: CardIcon },
+        { name: "Settings", icon: SettingsIcon },
+      ],
+      dashboardIcon: [DashboardIcon],
+    };
+  },
+};
 </script>
